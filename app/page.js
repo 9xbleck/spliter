6d46ee25226a14df1f3368b4f07339a9669b3d64
9xbleck <nabinkdl.999@gmail.com>
2025-11-05 17:40:35 +0545

    inngest-setup

--- app/page.js
+++ app/page.js
@@ -1,9 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <div className="">
-      <Button variant={"destructive"}>Click me</Button>
-    </div>
-  );
+  return <div className=""></div>;
 }
